9b279ad832dbb5555078aa5107f7059ca91ec759
GuillBuj <guillaume.bujon@gmail.com>
2025-07-02 16:13:42 +0200

    reorga + pom parent

--- src/main/java/com/medilabosolutions/patient_service/exception/PatientNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.medilabosolutions.patient_service.exception;
-
-public class PatientNotFoundException extends RuntimeException {
-    public PatientNotFoundException(Long id) {
-        super("Patient non trouvé avec l'ID : " + id);
-    }
-}
