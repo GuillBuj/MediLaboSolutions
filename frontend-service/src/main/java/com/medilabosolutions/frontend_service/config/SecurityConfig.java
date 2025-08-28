577cd2bad59a06993726381bc0e14177d4f89de4
GuillBuj <guillaume.bujon@gmail.com>
2025-08-28 09:47:43 +0200

    BCrypt

--- frontend-service/src/main/java/com/medilabosolutions/frontend_service/config/SecurityConfig.java
+++ frontend-service/src/main/java/com/medilabosolutions/frontend_service/config/SecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -33,15 +35,20 @@ public class SecurityConfig {
     }
 
     @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
         UserDetails admin = User.builder()
                 .username("admin")
-                .password("{noop}admin") // MVP
+                .password(passwordEncoder.encode("admin")) // MVP
                 .roles("ADMIN")
                 .build();
         UserDetails user = User.builder()
                 .username("user")
-                .password("{noop}user")
+                .password(passwordEncoder.encode("user"))
                 .roles("USER")
                 .build();
         return new InMemoryUserDetailsManager(admin, user);
